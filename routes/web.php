18356b9b73b1341900c075a2d569c711ec2f7323
Vincent Tanujaya <vincent.tanujaya@gmail.com>
2019-12-25 19:46:07 +0700

    ha

--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,68 @@ use Illuminate\Support\Facades\App;
 */
 App::singleton('oauth2', function() {
     $storage = new OAuth2\Storage\Pdo(array(
-        'dsn' => 'mysql:dbname=oauth2;host:localhost',
+        'dsn' => 'mysql:dbname=oauthlaravel;host:localhost',
         'username' => 'root',
         'password' => ''
     ));
-    $server = new OAuth2\Server($storage);
-    
+    $server = new OAuth2\Server($storage, array(
+        'allow_implicit' => true,
+    ));
     $server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));
     $server->addGrantType(new OAuth2\GrantType\UserCredentials($storage));
-    
+    $server->addGrantType(new OAuth2\GrantType\RefreshToken($storage));
+    $server->addGrantType(new OAuth2\GrantType\JwtBearer($storage,"localhost:8000"));
+    $server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));
     return $server;
 });
 
 Route::post('oauth/token', function(){
-    $bridgeRequest = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());
+    $bridgeRequest = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());    
     $bridgeResponse = new OAuth2\HttpFoundationBridge\Response();
     $bridgeResponse = App::make('oauth2')->handleTokenRequest($bridgeRequest,$bridgeResponse);
     return $bridgeResponse;
 });
 
-Route::post('oauth/token-verify', function(){
+Route::get('oauth/authorize', function(){
+    $bridgeRequest = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());    
+    $bridgeResponse = new OAuth2\HttpFoundationBridge\Response();
+    $bridgeResponse = App::make('oauth2')->handleAuthorizeRequest($bridgeRequest,$bridgeResponse, true);
+    dd($bridgeResponse);
+    return $bridgeResponse;
+});
+
+Route::get('oauth/registration/{client_id}', function($client_id){
     $bridgeRequest = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());
     $bridgeResponse = new OAuth2\HttpFoundationBridge\Response();
-    $bridgeResponse = App::make('oauth2')->verifyResourceRequest($bridgeRequest,$bridgeResponse);
+    $bridgeResponse = App::make('oauth2')->getClientInfo($bridgeRequest,$bridgeResponse,$client_id);
+    return $bridgeResponse;
+});
+
+Route::put('oauth/registration/{client_id}', function($client_id){
+    $bridgeRequest = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());
+    $bridgeResponse = new OAuth2\HttpFoundationBridge\Response();
+    $bridgeResponse = App::make('oauth2')->setClient($bridgeRequest,$bridgeResponse,$client_id);
+    return $bridgeResponse;
+});
+
+Route::delete('oauth/registration/{client_id}', function($client_id){
+    $bridgeRequest = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());
+    $bridgeResponse = new OAuth2\HttpFoundationBridge\Response();
+    $bridgeResponse = App::make('oauth2')->deleteClient($bridgeRequest,$bridgeResponse,$client_id);
+    return $bridgeResponse;
+});
+
+Route::post('oauth/registration', function(){
+    $bridgeRequest = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());
+    $bridgeResponse = new OAuth2\HttpFoundationBridge\Response();
+    $bridgeResponse = App::make('oauth2')->setClient($bridgeRequest,$bridgeResponse);
+    return $bridgeResponse;
+});
+
+Route::get('oauth/token-verify', function(){
+    $bridgeRequest = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());
+    $bridgeResponse = new OAuth2\HttpFoundationBridge\Response();
+    $bridgeResponse = App::make('oauth2')->getAccessTokenData($bridgeRequest,$bridgeResponse);
     return response()->json($bridgeResponse);
 });
 
